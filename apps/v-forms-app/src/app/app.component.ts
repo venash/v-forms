411d8c20186a1c77aa97a5500fc6e842501d7fbc
Vaclav Rasendorfer <venash@Vaclav-MacBook-Pro.local>
2021-03-02 15:23:13 +0100

    initial commit

---
+++ apps/v-forms-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'venash-forms-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'v-forms-app';
+}
